5944ed0ac1fa9ffdb849d140c5a968ae441ec1f7
nuxie <zkalinowska96@icloud.com>
2020-02-05 15:03:17 +0100

    Implemented bot's behaviour on start

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const bot = new SlackBot({
   token: "xoxb-933239002181-933697847524-Se0XsxOknVwERJpivNWEYezD",
   name: "626f74"
 });
+
+bot.on("start", () => {
+  const params = {
+    icon_emoji: ":rocket:"
+  };
+  bot.postMessageToChannel(
+    "bot",
+    "Tzzzzzt... What a wonderful day to be alive!",
+    params
+  );
+});
+
